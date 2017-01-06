dd85156b2f6ee1c3d8431152666dbca8ed5e26e1
Massimiliano Ranauro <huckbit@gmail.com>
2017-01-06 15:04:05 +0100

    get_template_part('content')

--- index.php
+++ index.php
@@ -4,24 +4,7 @@
 
         <?php if(have_posts()): ?>
         <?php while ( have_posts() ) : the_post(); ?>
-            <div class="blog-post">
-                <h2 class="blog-post-title">
-                    <a href="<?php the_permalink(); ?>">
-                        <?php the_title(); ?>
-                    </a>
-                </h2>
-                <p class="blog-post-meta">
-                    <?php the_time('F j, Y g:i a'); ?> <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' )); ?>"><?php the_author( ); ?></a>
-                </p>
-                <?php if(has_post_thumbnail( )): ?>
-                    <div class="post-thumb">
-                        <?php the_post_thumbnail( ); ?>
-                    </div>
-                <?php endif; ?>
-                <?php the_excerpt(); ?>
-            </div>
-            <!-- /.blog-post -->
-
+           <?php get_template_part( 'content'); ?> <!-- take post's content from content.php -->
         <?php endwhile; ?>
         <?php else : ?>
             <p><?php __('No Posts Found'); ?></p>
