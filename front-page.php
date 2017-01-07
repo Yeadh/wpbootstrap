5710eb2a3d98fa097579c2e314f30db91574dce1
Massimiliano Ranauro <huckbit@gmail.com>
2017-01-07 18:07:13 +0100

    customizer for social network

--- front-page.php
+++ front-page.php
@@ -80,8 +80,12 @@
 
 <footer class="blog-footer">
         <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo( 'name' ) ?> | <?php bloginfo( 'description' ) ?><p>
-            <a href="#">Back to top</a>
         </p>
+         <ul class="list-unstyled list-inline">
+            <li><a href="<?php echo get_theme_mod( 'facebook_profile_url', '#'); ?>" target="_blank"><i class="fa fa-facebook-official"></i> Facebook</a></li>
+            <li><a href="<?php echo get_theme_mod( 'twitter_profile_url', '#'); ?>" target="_blank"><i class="fa fa-twitter"></i> Twitter</a></li>
+        </ul>
+        <div class="text-right back-top"><a href="#">Back to top</a></div>
     </footer>
     <?php wp_footer(); ?>
 
--- inc/customizer.php
+++ inc/customizer.php
@@ -1,7 +1,8 @@
 <?php
     function wpb_customizer_register($wp_customize){
 
-        //Showcase Section
+        /* Showcase Section
+        -------------------------------------------------------- */ 
         $wp_customize->add_section('showcase', array(
             'title'         => __('Showcase', 'wpbootstrap'),
             'description'   => sprintf(__('Options for showcase', 'wpbootstrap')),
@@ -68,6 +69,39 @@
             'section'   => 'showcase',
             'priority'  => 5
         ));
-    }
 
+        /* Social Networks Section
+        -------------------------------------------------------- */ 
+        $wp_customize->add_section('social', array(
+            'title'         =>__('Social Networks', 'wpbootstrap'),
+            'description'   => sprintf(__('Social network profiles','wpbootstrap')),
+            'priority'      => 150
+        ));
+
+        //facebook
+        $wp_customize->add_setting('facebook_profile_url', array(
+            'default'   => _x('https://www.facebook.com/profileName', 'wpbootstrap' ),
+            'type'      => 'theme_mod'
+        ));
+        $wp_customize->add_control('facebook_profile_url', array(
+            'label'     =>__('Facebook', 'wpbootstrap'),
+            'section'   => 'social',
+            'priority'  => 1
+        ));
+
+
+        //Twitter
+        $wp_customize->add_setting('twitter_profile_url', array(
+            'default'   => _x('https://twitter.com/profileName', 'wpbootstrap' ),
+            'type'      => 'theme_mod'
+        ));
+        $wp_customize->add_control('twitter_profile_url', array(
+            'label'     =>__('Twitter', 'wpbootstrap'),
+            'section'   => 'social',
+            'priority'  => 2
+        ));
+
+
+    }
+    
     add_action( 'customize_register', 'wpb_customizer_register' );
