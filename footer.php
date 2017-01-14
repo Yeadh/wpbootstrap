1d01e50a2f9634627c1e3fd7bfed99aa0eddeaf1
Massimiliano Ranauro <huckbit@gmail.com>
2017-01-14 20:26:22 +0100

    jquery added

--- footer.php
+++ footer.php
@@ -14,7 +14,6 @@
     </footer>
     <?php wp_footer(); ?>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </body>
 
 </html>
--- inc/front/enqueue.php
+++ inc/front/enqueue.php
@@ -2,7 +2,7 @@
 
 function wpb_enqueue(){
 
-    //Style and fonts
+//Style and fonts
     wp_register_style('wpb_boostrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
     wp_register_style('style', get_template_directory_uri() . '/style.css');
 
@@ -10,10 +10,12 @@ function wpb_enqueue(){
     wp_enqueue_style( 'style' );
 
 
-    //Javascript
+//Javascript
+    wp_register_script( 'jQuery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', NULL, false, true );
     wp_register_script( 'wpb_boostrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(jQuery), false, true);
-    wp_register_script( 'wpb_main_js', get_template_directory_uri() . '/js/main.js');
+    wp_register_script( 'wpb_main_js', get_template_directory_uri() . '/js/main.js', array(jQuery), false, true );
     
+    wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'wpb_boostrap' );
     wp_enqueue_script( 'wpb_main_js' );
 
