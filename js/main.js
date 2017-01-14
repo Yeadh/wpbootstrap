29f1a94f70099db900a2695fe0c1c046cc380fab
Massimiliano Ranauro <huckbit@gmail.com>
2017-01-14 21:21:31 +0100

    API implemented

--- js/main.js
+++ js/main.js
@@ -1,2 +1,41 @@
 console.log('JavaScript works');
 
+var portfolioPostsBtn = document.getElementById('portfolio-post-btn');
+var portfolioPostsContainer = document.getElementById('portfolio-posts-container');
+
+
+if(portfolioPostsBtn){
+
+    portfolioPostsBtn.addEventListener("click", function(){
+
+        var ourRequest = new XMLHttpRequest();
+
+        ourRequest.open('GET', 'http://localhost:8888/wpDevTest/wp-json/wp/v2/posts?categories=8');
+        ourRequest.onload = function() {
+            if (ourRequest.status >= 200 && ourRequest.status < 400) {
+                var data = JSON.parse(ourRequest.responseText);
+                console.log(data);
+                createHTML(data);
+            } else {
+                console.log("We connected to the server, but it returned an error.");
+            }
+        };
+
+        ourRequest.onerror = function() {
+            console.log("Connection error");
+        };
+
+        ourRequest.send();
+
+    });
+
+}
+
+function createHTML(postData){
+    var ourHTMLString = '';
+    for(i=0; i < postData.length; i++) {
+        ourHTMLString += '<h2>' + postData[i].title.rendered + '</h2>';
+        ourHTMLString += '<p>' + postData[i].content.rendered + '</p>';
+    }
+    portfolioPostsContainer.innerHTML = ourHTMLString;
+}
---
+++ page-portfolio.php
@@ -0,0 +1,35 @@
+<?php 
+/**
+ * Template Name: Portfolio
+ *
+ * @package WordPress
+ * @subpackage wpbootstrap
+ * @since wpbootstrao v1.0
+ */
+
+?>
+<?php get_header( ); ?>
+    <div class="row">
+        <div class="col-sm-8 blog-main">
+
+        <?php if(have_posts()): ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <div class="blog-post">
+                <h2 class="blog-post-title">
+                    <?php the_title(); ?>
+                </h2>
+                <?php the_content(); ?>
+                <button id="portfolio-post-btn" class="btn btn-primary btn-sm">Load Portfolio related blog post</button>
+                <div id="portfolio-posts-container"></div>
+            </div>
+            <!-- /.blog-post -->
+
+        <?php endwhile; ?>
+        <?php else : ?>
+            <p><?php __('No Page Found'); ?></p>
+        <?php endif; ?>
+
+    </div><!-- /.blog-main -->
+
+<?php get_sidebar(); ?>   
+<?php get_footer( ) ?>
