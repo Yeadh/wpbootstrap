7385f87e1fdc0de69657140bb1d2527cb070a17e
Massimiliano Ranauro <huckbit@gmail.com>
2017-02-01 11:25:46 +0100

    full page post

---
+++ page-full-width.php
@@ -0,0 +1,33 @@
+<?php 
+/**
+ * Template Name: Page Full Width
+ *
+ * @package WordPress
+ * @subpackage wpbootstrap
+ * @since wpbootstrao v1.0
+ */
+
+?>
+<?php get_header( ); ?>
+    <div class="row">
+        <div class="col-sm-12 blog-main">
+
+        <?php if(have_posts()): ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <div class="blog-post">
+                <h2 class="blog-post-title">
+                    <?php the_title(); ?>
+                </h2>
+                <?php the_content(); ?>
+                <div id="portfolio-posts-container"></div>
+            </div>
+            <!-- /.blog-post -->
+
+        <?php endwhile; ?>
+        <?php else : ?>
+            <p><?php __('No Page Found'); ?></p>
+        <?php endif; ?>
+
+    </div><!-- /.row -->
+
+<?php get_footer( ) ?>
